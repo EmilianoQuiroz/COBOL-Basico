61a6cdeac80e4507112e09f3d82236a7a573f0b9
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-03-29 17:17:40 -0300

    Primer programa en COBOL

---
+++ HelloWorld.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author:Santiago Quiroz
+      * Date:29/03/22
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            DISPLAY "Primer programa en COBOL"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
